53fc2e3102bbd23a59ec05ab00fcd3e2c56b49f2
TomazMartins <tomaz.unb@outlook.com>
2015-06-12 19:46:52 -0300

    Adding bootstrap
    Signed-off-by: TomazMartins <tomaz.unb@outlook.com>

--- gepetohut/app/assets/javascripts/application.js
+++ gepetohut/app/assets/javascripts/application.js
@@ -13,4 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require twitter/bootstrap
+//= require twitter/bootstrap/transition
+//= require twitter/bootstrap/alert
+//= require twitter/bootstrap/modal
+//= require twitter/bootstrap/button
+//= require twitter/bootstrap/collapse
 //= require_tree .
