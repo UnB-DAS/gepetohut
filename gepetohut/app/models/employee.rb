8d0e94101976df33a8c273f96e1570e241516105
Jose Pedro <1jpsneto@gmail.com>
2015-06-14 02:44:02 -0300

    Fix conflits on employee and restaurants model

--- gepetohut/app/models/employee.rb
+++ gepetohut/app/models/employee.rb
@@ -1,6 +1,3 @@
 class Employee < ActiveRecord::Base
-<<<<<<< HEAD
-=======
   belongs_to :restaurant
->>>>>>> maintain_employee#10
 end
--- gepetohut/app/models/restaurant.rb
+++ gepetohut/app/models/restaurant.rb
@@ -1,6 +1,3 @@
 class Restaurant < ActiveRecord::Base
-<<<<<<< HEAD
-=======
   has_many :employees, dependent: :destroy
->>>>>>> maintain_employee#10
 end
