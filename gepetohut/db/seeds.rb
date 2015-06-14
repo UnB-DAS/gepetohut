016f4d9c02d83ce6c02ab1dd0e415541805838fe
TomazMartins <tomaz.unb@outlook.com>
2015-06-14 11:30:10 -0300

    Adding entities to db/seeds.rb
    Signed-off-by: TomazMartins <tomaz.unb@outlook.com>

--- gepetohut/db/seeds.rb
+++ gepetohut/db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# PIZZAS
 #
-# Examples:
+pizzas = Pizzas.create(
+  [
+    { name: "Alho e Óleo", price: 26.00 },
+    { name: "Americana", price: 24.00 },
+    { name: "Atum", price: 24.00 },
+    { name: "Bacon", price: 22.00 },
+    { name: "Baiana", price: 25.00 },
+    { name: "Bolonhesa", price: 22.00 },
+    { name: "Brasileira", price: 25.00 },
+    { name: "Brócolis", price: 21.00 },
+    { name: "Calabresa", price: 20.00 },
+    { name: "Champignon", price: 25.00 },
+    { name: "Frango", price: 21.00 },
+    { name: "Frango com Catupiry", price: 25.00 },
+    { name: "Lombo", price: 25.00 },
+    { name: "Marguerita", price: 22.00 },
+    { name: "Milho", price: 21.00 },
+    { name: "Mussarela", price: 18.00 },
+    { name: "Palmito", price: 25.00 },
+    { name: "Portuguesa", price: 25.00 },
+    { name: "Vegetariana", price: 22.00 }
+  ]
+)
+
+# RESTAURANTS
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+restaurants = Restaurant.create(
+  [
+    { name: "GepetoHut: Taguatinga Sul", qtd_delivery: 5, qtd_pizzaiolo: 5 },
+    { name: "GepetoHut: Taguatinga Norte", qtd_delivery: 5, qtd_pizzaiolo: 6 },
+    { name: "GepetoHut: Taguatinga Centro", qtd_delivery: 7, qtd_pizzaiolo: 6 }
+  ]
+)
