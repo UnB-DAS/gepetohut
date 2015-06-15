af3ac9c68a0cac4e7c68bd4a5714cae954ad285c
Jose Pedro <1jpsneto@gmail.com>
2015-06-15 18:46:22 -0300

    Adding pizzas to get order

--- gepetohut/app/controllers/make_order_controller.rb
+++ gepetohut/app/controllers/make_order_controller.rb
@@ -1,4 +1,9 @@
 class MakeOrderController < ApplicationController
   def index
+  	@pizzas = Pizza.all
+  end
+
+  def finish_order
+    render "app/views/home_page/index"
   end
 end
--- gepetohut/config/routes.rb
+++ gepetohut/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'make_order/index'
+  get 'make_order/finish_order'
 
   devise_for :users
 
