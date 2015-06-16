7efde96f305ffddb6b8b66842d34f0f4f9631359
Jose Pedro <1jpsneto@gmail.com>
2015-06-15 22:29:29 -0300

    Fixing bug in relationship between pizzas and orders

--- gepetohut/app/controllers/home_page_controller.rb
+++ gepetohut/app/controllers/home_page_controller.rb
@@ -7,7 +7,7 @@ class HomePageController < ApplicationController
 
 
   	unless params_pizzas.values.length == params_pizzas.values.count("0")
-  		@order = Order.new
+  		@order = Order.create
   		@order.total_to_pay = 0
   		@order.qtd_pizzas = 0
   		pizzas_id = params_pizzas.keys
@@ -15,17 +15,14 @@ class HomePageController < ApplicationController
   		quantity_pizza = params_pizzas.values
 		# get whats pizzas that client want
 		pizzas_id.each do |pizza_id|
-			puts "aidhasiduhaisuhdiasuhdiashdihas"
-			
-			
 			# get quantity of pizzas that client want
-			quantity_pizza.each do |quantity|
-				quantity = quantity.to_i
-				for i in 1 .. quantity
+			if params_pizzas[pizza_id.to_s].to_i != 0
+				for i in 1 .. params_pizzas[pizza_id.to_s].to_i
 					@pizza = Pizza.find(pizza_id)
 					@order.pizzas << @pizza
 					@order.total_to_pay = @order.total_to_pay + @pizza.price
 					@order.qtd_pizzas += 1
+					@order.save
 				end
 			end
 		end
--- gepetohut/app/models/pizza.rb
+++ gepetohut/app/models/pizza.rb
@@ -1,6 +1,7 @@
 class Pizza < ActiveRecord::Base
   has_and_belongs_to_many :ingredients
   belongs_to :order
+  has_many :orders
 
   PRESENT = true
 
