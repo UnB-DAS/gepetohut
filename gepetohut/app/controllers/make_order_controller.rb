8843ea123dd828ec1e32372223bb2dfef369942e
Jose Pedro <1jpsneto@gmail.com>
2015-06-25 20:01:27 -0300

    Adding list of orders and pizzas to costumer

--- gepetohut/app/controllers/make_order_controller.rb
+++ gepetohut/app/controllers/make_order_controller.rb
@@ -4,12 +4,45 @@ class MakeOrderController < ApplicationController
   
   def index
     @pizzas = Pizza.all
-
   end
 
   def make_order
-  	puts "69"*100
-  	puts params
-  	render "home_page/index"
+  	params_pizzas = params
+  	params_pizzas.delete "utf8"
+  	params_pizzas.delete "controller"
+  	params_pizzas.delete "action"
+
+  	# if we have some pizza to pay
+  	unless params_pizzas.values.length == params_pizzas.values.count("0")
+  		@order = current_user.orders.build
+
+  		@order.total_to_pay = 0
+  		@order.qtd_pizzas = 0
+  		pizzas_id = params_pizzas.keys
+  		quantity_pizza = params_pizzas.values
+		# get whats pizzas that client want
+		pizzas_id.each do |pizza_id|
+			# get quantity of pizzas that client want
+			if params_pizzas[pizza_id.to_s].to_i != 0
+				for i in 1 .. params_pizzas[pizza_id.to_s].to_i
+					@pizza = Pizza.find(pizza_id)
+					puts "=D"*100
+					puts @pizza.name
+					puts "=D"*100
+					@pizza.quantity = params_pizzas[pizza_id.to_s].to_i
+					@order.pizzas << @pizza
+					puts "=D"*100
+					puts @order.pizzas.last.name
+					puts "=D"*100
+					@order.total_to_pay = @order.total_to_pay + @pizza.price
+					@order.qtd_pizzas = @order.qtd_pizzas + 1
+				end
+			end
+		@order.save
+		end
+  	end
+
+  	redirect_to home_page_index_path
+  	#render "home_page/index"
   end
 end
