d2b0f2820465215d168a000478a140179a99d3f9
Thaiane <thaiane.unb@hotmail.com>
2015-06-29 17:36:23 -0300

    Validade presence in evaluation

--- gepetohut/app/models/evaluation.rb
+++ gepetohut/app/models/evaluation.rb
@@ -1,5 +1,6 @@
 class Evaluation < ActiveRecord::Base
-  belongs_to :customer
   belongs_to :user
   belongs_to :order
+
+  validates_presence_of :rating_pizzeria, :rating_service, :rating_pizza, :description
 end
