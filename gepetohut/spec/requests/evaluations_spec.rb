348c2d5a4ab173f99cfdcabccdae9ef217e423c1
MaxAlmeida <llewxam100@hotmail.com>
2015-06-30 01:05:01 -0300

    Adding sign_in in evaluation request

--- gepetohut/spec/requests/evaluations_spec.rb
+++ gepetohut/spec/requests/evaluations_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
-
+require 'shared_contexts'
 RSpec.describe "Evaluations", type: :request do
+  include_context "api request authentication helper methods"
+  include_context "api request global before and after hooks"
+   before :each do
+      roles = [ "customer", "manager", "admin" ]
+      roles.each do |role|
+            Role.find_or_create_by( { name: role } )
+        end
+      @user = FactoryGirl.create(:admin)
+  end
   describe "GET /evaluations" do
     it "works! (now write some real specs)" do
+      sign_in @user
       get evaluations_path
       expect(response).to have_http_status(200)
     end
