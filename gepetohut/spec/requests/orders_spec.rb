0d51965e1660c5c41cfdc67ce0c3c28e76e88c9c
MaxAlmeida <llewxam100@hotmail.com>
2015-06-30 01:05:54 -0300

    Adding sign_in in order spec in request

--- gepetohut/spec/requests/orders_spec.rb
+++ gepetohut/spec/requests/orders_spec.rb
@@ -2,7 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Orders", type: :request do
   describe "GET /orders" do
+    include_context "api request authentication helper methods"
+    include_context "api request global before and after hooks"
+    before :each do
+      roles = [ "customer", "manager", "admin" ]
+      roles.each do |role|
+            Role.find_or_create_by( { name: role } )
+        end
+      @user = FactoryGirl.create(:admin)
+  end
     it "works! (now write some real specs)" do
+      sign_in(@user)
       get orders_path
       expect(response).to have_http_status(200)
     end
