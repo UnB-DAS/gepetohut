b6eba97498f5c5bc8b7fbeffa7b9e88e43bc2053
MaxAlmeida <llewxam100@hotmail.com>
2015-06-30 01:04:17 -0300

    Adding log in admin in employees request

--- gepetohut/spec/requests/employees_spec.rb
+++ gepetohut/spec/requests/employees_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
-
+require 'shared_contexts'
 RSpec.describe "Employees", type: :request do
+  include_context "api request authentication helper methods"
+  include_context "api request global before and after hooks"
+   before :each do
+      roles = [ "customer", "manager", "admin" ]
+      roles.each do |role|
+            Role.find_or_create_by( { name: role } )
+        end
+      @user = FactoryGirl.create(:admin)
+  end
   describe "GET /employees" do
     it "works! (now write some real specs)" do
+      sign_in(@user)
       get employees_path
       expect(response).to have_http_status(200)
     end
   end
 end
+
+
