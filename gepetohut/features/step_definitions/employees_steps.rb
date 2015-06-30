691b3723b094e2c3c8a2a91215cd88cd1e53db63
Thaiane <thaiane.unb@hotmail.com>
2015-06-29 23:10:12 -0300

    Solving conflicts and updating branch

---
+++ gepetohut/features/step_definitions/employees_steps.rb
@@ -0,0 +1,23 @@
+Quando( /^eu entro na página inicial de funcionários$/) do
+  visit employees_path
+end
+
+E( /^eu clico e acesso a página de cadastro de funcionários$/) do
+  click_button( "Cadastrar Novo Funcionário" )
+end
+
+E( /^eu preencho o campo nome do funcionário com (.*)$/) do |name_employee|
+  page.fill_in( "name-employee", with: name_employee )
+end
+
+E( /^eu preencho o campo CPF com (.*)$/) do |cpf_employee|
+  page.fill_in( "cpf-employee", with: cpf_employee )
+end
+
+E( /^eu preencho o campo rg com (.*)$/) do |rg_employee|
+  page.fill_in( "rg-employee", with: rg_employee )
+end
+
+E(/^eu clico no botão de criação de Cadastrar Dados do Funcionário$/) do
+  click_button( "Cadastrar Dados do Funcionário" )
+end
